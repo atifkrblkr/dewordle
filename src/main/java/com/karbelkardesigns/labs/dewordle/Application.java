217b39a3a2f8053ab3812c2ca398dbebd8884aa4
atifkarbelkar <atif.krblkr@gmail.com>
2022-03-01 13:08:15 -0500

    Deployment failure fix

--- src/main/java/com/karbelkardesigns/labs/dewordle/Application.java
+++ src/main/java/com/karbelkardesigns/labs/dewordle/Application.java
@@ -2,15 +2,19 @@ package com.karbelkardesigns.labs.dewordle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws IOException {
-        try(BufferedReader br = new BufferedReader(new FileReader(ResourceUtils.getFile("classpath:words.txt")))){
+        ClassPathResource r = new ClassPathResource("words.txt");
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(r.getInputStream()))) {
             Dictionary.instance.allWords.addAll(br.lines().filter(word -> word.length() == 5).collect(Collectors.toList()));
         }
         SpringApplication.run(Application.class, args);
